d71a748bd2e2f1a2ee7104fb2d9e6510715be5f4
austinirvine <aa1irvine@gmail.com>
2019-10-12 15:43:11 -0500

    Change index file

---
+++ build/precache-manifest.a3b8a3e61e27205353a4742d3a8c13f9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b331652058ca0169a8c729b61155931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "007012e8afbe6d11e5f6",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "3564cc17c5ca79249add",
+    "url": "/static/css/main.c3f82f2c.chunk.css"
+  },
+  {
+    "revision": "007012e8afbe6d11e5f6",
+    "url": "/static/js/2.c9275405.chunk.js"
+  },
+  {
+    "revision": "3564cc17c5ca79249add",
+    "url": "/static/js/main.9ce95c20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c1e5eb758b969677a75c90f19455457",
+    "url": "/static/media/1.3c1e5eb7.png"
+  },
+  {
+    "revision": "46d280aafbfb10946cb9b6348544edc5",
+    "url": "/static/media/Favicon.46d280aa.jpg"
+  },
+  {
+    "revision": "96cefcce93f7183657aedb55ff57626b",
+    "url": "/static/media/GENPINK.96cefcce.png"
+  },
+  {
+    "revision": "2bf52376d2635752877e5f35e5227823",
+    "url": "/static/media/IMG_1102.2bf52376.jpg"
+  },
+  {
+    "revision": "41acef7ee27e4f1b0e7361b52805c154",
+    "url": "/static/media/IMG_1805.41acef7e.JPG"
+  },
+  {
+    "revision": "a0e965b5969c163163ab19ac6d6db12b",
+    "url": "/static/media/IMG_1808.a0e965b5.jpg"
+  },
+  {
+    "revision": "bb903e83fa87f124f5ca62c786c54e1f",
+    "url": "/static/media/IMG_1810.bb903e83.PNG"
+  },
+  {
+    "revision": "d656c791153d513159c96aec6efabd5a",
+    "url": "/static/media/IMG_5848.d656c791.jpg"
+  },
+  {
+    "revision": "34f546e6fde27002d394e4a5f5ba2f22",
+    "url": "/static/media/OriginalKnob.34f546e6.png"
+  },
+  {
+    "revision": "312a6693990b8a33230ae95ceb95cc64",
+    "url": "/static/media/PocketEggy2.312a6693.png"
+  },
+  {
+    "revision": "5e485ab37fdcc8b51ef6fd7f73b58e48",
+    "url": "/static/media/RussianDoll.5e485ab3.png"
+  },
+  {
+    "revision": "49dabfb648c82c68aeb016b0bc44709a",
+    "url": "/static/media/bluegirl.49dabfb6.png"
+  },
+  {
+    "revision": "57a6be4ec25aa598596dba2886ec40d0",
+    "url": "/static/media/eldridge.57a6be4e.png"
+  },
+  {
+    "revision": "bbe3a3e5c45e1c97d3653cdabcce8367",
+    "url": "/static/media/google.bbe3a3e5.jpg"
+  }
+]);
